1df1e748870acf97956d1d8611bc365a1ac20471
myl7 <myl@myl.moe>
2024-08-22 20:55:39 +0800

    Fix build and init config

--- build/package/pack.sh
+++ build/package/pack.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+mkdir -p out
 make DESTDIR=out/deb/cityu-cp37-keep-online prefix=/ exec_prefix=/usr install-deb
 dpkg-deb --build out/deb/cityu-cp37-keep-online
